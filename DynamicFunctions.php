ac07c047f91b611e01a66a6b82e8930fd270f4ac
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-09-12 15:33:02 +0200

    remove unused functions update extension credits

--- DynamicFunctions.php
+++ DynamicFunctions.php
@@ -1,89 +1,56 @@
 <?php
-/*
 
- Defines a subset of parser functions that must clear the cache to be useful.
+/*
+ Defines a parser function that allows to access URL arguments.
+ Based on the DynamicFunctions extension made by RossMcClure a.k.a. Algorothm.
 
  {{#arg:name}} Returns the value of the given URL argument.  Can also be called
                with a default value, which is returned if the given argument is
                undefined or blank: {{#arg:name|default}}
 
- {{#ip:}}      Returns the current user IP.
-
- {{#rand:a|b}} Returns a random value between a and b, inclusive.  Can
-               also be called with a single value; {{#rand:6}} returns a
-               random value between 1 and 6 (equivalent to a dice roll).
-
- {{#skin:}}    Returns the name of the current skin.
-
- Author: Algorithm [http://meta.wikimedia.org/wiki/User:Algorithm]
+ Author: Algorithm, MrMebelMan [http://meta.wikimedia.org/wiki/User:Algorithm, https://github.com/MrMebelMan]
  Version 1.1 (11/25/06)
 
 */
 
-# Not a valid entry point, skip unless MEDIAWIKI is defined
 if ( !defined( 'MEDIAWIKI' ) ) {
    die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-$wgExtensionFunctions[] = 'wfDynamicFunctions';
+$wgExtensionFunctions[] = 'wfUrlArguments';
+
 $wgExtensionCredits['parserhook'][] = array(
-	'name' => 'DynamicFunctions',
-	'version' => '1.1',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:DynamicFunctions',
-	'author' => 'Ross McClure',   
-	'description' => 'Defines an additional set of parser functions.'
+	'name' => 'UrlArguments',
+	'version' => '1.0',
+	'url' => 'https://github.com/MrMebelMan/mediawiki-extensions-UrlArguments',
+	'author' => 'Ross McClure, MrMebelMan',   
+	'description' => 'Defines an additional parser function that allows to access URL arguments.'
 );
 
-$wgHooks['LanguageGetMagic'][] = 'wfDynamicFunctionsLanguageGetMagic';
+$wgHooks['LanguageGetMagic'][] = 'wfUrlArgumentsLanguageGetMagic';
 
-function wfDynamicFunctions() {
-	global $wgParser, $wgExtDynamicFunctions;
+function wfUrlArguments() {
+	global $wgParser, $wgExtUrlArguments;
 
-	$wgExtDynamicFunctions = new ExtDynamicFunctions();
+	$wgExtUrlArguments = new ExtUrlArguments();
 
-	$wgParser->setFunctionHook( 'arg', array( &$wgExtDynamicFunctions, 'arg' ) );
-	$wgParser->setFunctionHook( 'ip', array( &$wgExtDynamicFunctions, 'ip' ) );
-	$wgParser->setFunctionHook( 'rand', array( &$wgExtDynamicFunctions, 'rand' ), SFH_OBJECT_ARGS );
-	$wgParser->setFunctionHook( 'skin', array( &$wgExtDynamicFunctions, 'skin' ) );
+	$wgParser->setFunctionHook( 'arg', array( &$wgExtUrlArguments, 'arg' ) );
 }
 
-function wfDynamicFunctionsLanguageGetMagic( &$magicWords, $langCode ) {
+function wfUrlArgumentsLanguageGetMagic( &$magicWords, $langCode ) {
 	switch ( $langCode ) {
-	default:
-		$magicWords['arg']    = array( 0, 'arg' );
-		$magicWords['ip']     = array( 0, 'ip' );
-		$magicWords['rand']   = array( 0, 'rand' );
-		$magicWords['skin']   = array( 0, 'skin' );
+		default:
+			$magicWords['arg']    = array( 0, 'arg' );
 	}
 	return true;
 }
 
-class ExtDynamicFunctions {
-
+class ExtUrlArguments {
 	function arg( &$parser, $name = '', $default = '' ) {
 		global $wgRequest;
 		$parser->disableCache();
 		return $wgRequest->getVal($name, $default);
 	}
-
-	function ip( &$parser ) {
-		$parser->disableCache();
-		return wfGetIP();
-	}
-
-	function rand( &$parser, $frame, $args ) {
-		$a = isset( $args[0] ) ? $frame->expand( $args[0] ) : 0;
-		$b = isset( $args[1] ) ? $frame->expand( $args[1] ) : 1;
-		$parser->disableCache();
-		if ( is_callable( array( $frame, 'setVolatile' ) ) ) {
-			$frame->setVolatile(); // see bug #58929
-		}
-		return mt_rand( intval($a), intval($b) );
-	}
-
-	function skin( &$parser ) {
-		global $wgUser, $wgRequest;
-		$parser->disableCache();
-		return $wgRequest->getVal('useskin', $wgUser->getOption('skin'));
-	}
 }
+
+?>
